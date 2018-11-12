8d8aceb728883dddfc22a579c0a20d06e225fef6
Brandon Tran <brandontran@Brandons-MacBook-Pro.local>
2018-11-12 00:19:56 -0800

    Replaced isDrowsy stub with drowsiness detection using eye aspect ratio. Commented out call to alert function to run while sound modules aren't installed. Included blinking threshold and eye aspect ratio threshold constants, and eye aspect ratio function.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 #Run this file by its self to test for facial detection with a static image
-
+from scipy.spatial import distance as dist
 from imutils.video import VideoStream
 from imutils import face_utils
 import numpy as np
@@ -15,7 +15,36 @@ test = False
 testFailed = 0
 
 shape_predictor_file = "shape_predictor_68_face_landmarks.dat"
-frameRate =  4
+frameRate =  60
+
+# define two constants, one for the eye aspect ratio to indicate
+# blink and then a second constant for the number of consecutive
+# frames the eye must be below the threshold for to set off the
+# alarm
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES = 48
+COUNTER = 0
+
+# grab the indexes of the facial landmarks for the left and
+# right eye, respectively
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+def eye_aspect_ratio(eye):
+	# compute the euclidean distances between the two sets of
+	# vertical eye landmarks (x, y)-coordinates
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
+
+	# compute the euclidean distance between the horizontal
+	# eye landmark (x, y)-coordinates
+	C = dist.euclidean(eye[0], eye[3])
+
+	# compute the eye aspect ratio
+	ear = (A + B) / (2.0 * C)
+
+	# return the eye aspect ratio
+	return ear
 
 #Grabs frame from the video source
 def grabFrame(vs):
@@ -89,13 +118,42 @@ def main(webcamSource):
 
 			frame = drawBox(frame, rect)
 
-			#Check if the person is drowsy
-			if isDrowsy.isDrowsy(shape):
-				alertUser = True
-				cv2.putText(frame, "DROWSINESS ALERT!", (10, 30), 
-					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+			# extract the left and right eye coordinates, then use the
+			# coordinates to compute the eye aspect ratio for both eyes
+			leftEye = shape[lStart:lEnd]
+			rightEye = shape[rStart:rEnd]
+			leftEAR = eye_aspect_ratio(leftEye)
+			rightEAR = eye_aspect_ratio(rightEye)
 
-		alerts.alert_value(alertUser)
+			# average the eye aspect ratio together for both eyes
+			ear = (leftEAR + rightEAR) / 2.0
+
+			# compute the convex hull for the left and right eye, then
+			# visualize each of the eyes
+			leftEyeHull = cv2.convexHull(leftEye)
+			rightEyeHull = cv2.convexHull(rightEye)
+			cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+			cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+			
+			#Check if the person is drowsy
+			# check to see if the eye aspect ratio is below the blink
+			# threshold, and if so, increment the blink frame counter
+			if ear < EYE_AR_THRESH:
+				COUNTER += 1
+
+				# if the eyes were closed for a sufficient number of
+				# then sound the alarm
+				if COUNTER >= EYE_AR_CONSEC_FRAMES:
+					alertUser = True
+					cv2.putText(frame, "DROWSINESS ALERT!", (10, 30), 
+						cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+			
+			# otherwise, the eye aspect ratio is not below the blink
+			# threshold, so reset the counter and alarm
+			else:
+				COUNTER = 0
+
+		#alerts.alert_value(alertUser)
 
 		cv2.imshow("Frame", frame)
 		key = cv2.waitKey(1) & 0xFF
