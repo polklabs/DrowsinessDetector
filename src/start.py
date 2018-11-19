75a7a426a7224d8c27f6d2e35b969eec88dadedc
David Sun <davidsun@umail.ucsb.edu>
2018-11-19 13:20:07 -0800

    Updated logo.  If button closes main subprocess if main is running.

--- src/start.py
+++ src/start.py
@@ -2,6 +2,9 @@ import initialize
 
 # Unique button process class
 from subprocess import Popen
+import atexit
+import time
+import sys
 
 # Have a button that starts the program
 import Tkinter
@@ -10,25 +13,36 @@ from PIL import Image, ImageTk
 if __name__ == '__main__':
 
    haveOpened = False
-   AEROSPACE_LOGO = "../assets/logo.jpg"
+   AEROSPACE_LOGO = "../assets/logo.png"
    A_LOGO = "../assets/A_logo.jpg"
    webcameraSource = initialize.initialize()
-
+   all_processes = []
    # Runs a subprocess if there isn't one running
    # Otherwise polls to see if there is a subprocess to kill
    def helloCallBack():
       global haveOpened
       if(haveOpened == False):
-         global p
+         global p, all_processes
          p = Popen(["python", "button_popen.py", str(webcameraSource)])
+         all_processes.append(p)
          haveOpened = True
       else:
          poll = p.poll()
          if (poll == None):
+            all_processes.remove(p)
             p.kill()
          else:
             p = Popen(["python", "button_popen.py", str(webcameraSource)])
+            all_processes.append(p)
 
+   def cleanup():
+      global all_processes
+      if len(all_processes) != 0:
+         for p in all_processes:  # list of your processes
+            if isinstance(p, Popen) and p.poll() == None:
+               p.kill()
+
+   atexit.register(cleanup)
 
    top = Tkinter.Tk()
 
@@ -40,8 +54,12 @@ if __name__ == '__main__':
    top.resizable(False, False)
    top.iconbitmap(default="../assets/icon.ico")
 
-   B = Tkinter.Button(top,image=photo, height =100,width=800,
+   B = Tkinter.Button(top,image=photo, height =100,width=450,
                       command=helloCallBack)
 
    B.pack()
    top.mainloop()
+
+
+
+
