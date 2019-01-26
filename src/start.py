51c341ac54e989cfac1b784fe35ced43fb299ab4
David Sun <davidsun@umail.ucsb.edu>
2019-01-26 15:04:02 -0800

    Basic firebase implementation
    Nothing really has been fully tested

--- src/start.py
+++ src/start.py
@@ -40,6 +40,7 @@ if __name__ == '__main__':
          haveOpened = True
          widget['background'] = 'red'
          widget['text'] = 'STOP'
+         isRunning = True
       else:
          poll = p.poll()
          if (poll == None):
@@ -47,11 +48,13 @@ if __name__ == '__main__':
             p.kill()
             widget['background'] = 'green'
             widget['text'] = 'START'
+            isRunning = False
          else:
             p = Popen(["python", "button_popen.py", str(webcameraSource)])
             all_processes.append(p)
             widget['background'] = 'red'
             widget['text'] = 'STOP'
+            isRunning = True
 
    # Kills all subprocesses that are running if there are any
    # Also sets isRunning to False so the thread can stop
@@ -73,6 +76,7 @@ if __name__ == '__main__':
                if isinstance(p,Popen) and p.poll() != None:
                   widget['background'] = 'green'
                   widget['text'] = 'START'
+                  isRunning = False
                   # Honestly a horrible idea.
                   # If the array isn't always size 0 or 1 this could lead to some real issues
                   all_processes.remove(p)
